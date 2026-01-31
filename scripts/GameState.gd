bb6ef546e6414d628f352253c9293a36bd8c53a3
Richard Wepner <richard.wepner@gmx.net>
2026-01-31 14:50:55 +0100

    minor cleanup

--- scripts/GameState.gd
+++ scripts/GameState.gd
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:57707605955750b797b82ee33e8e36f9037b48f540a3859f3186ca3fe3410ffc
-size 3074
+oid sha256:5995db6e513166b67d0c56d52894bb3ab57593a1dc8b9fbdecdaa4ca0d3923cb
+size 2168
