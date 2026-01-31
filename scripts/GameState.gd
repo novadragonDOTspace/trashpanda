f215b08afe8d764148f28d53d1e01cfd933f7366
Richard Wepner <richard.wepner@gmx.net>
2026-01-31 11:34:41 +0100

    minor adjustments to presentation and production calculation

--- scripts/GameState.gd
+++ scripts/GameState.gd
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:561e0b3813072c416017590279a5f66f34e4f395a757b3a32bf48a056ae6367b
-size 3593
+oid sha256:5701a251f71758bfca370886c2f7bf1cbf5b9cf19a016ba7dd284472aeca8884
+size 2982
--- scripts/StatePrersentation.gd
+++ scripts/StatePrersentation.gd
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:1a60dffe90f0722af8969050c13d249bbb19576fd46e80df88feafe362a11dec
-size 1058
+oid sha256:a499a7d1440a439993700fc4201ea205de2cf687226b1fcf011e654f7b77bc8b
+size 1353
