5c170e4d74e6762fbab53c8055c1242f5a2e2910
Richard Wepner <richard.wepner@gmx.net>
2026-01-31 12:03:58 +0100

    added automatic time advancement

--- scripts/GameState.gd
+++ scripts/GameState.gd
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:d340c8d8578de1c75b06e328f12d53b3dd2fca919c7098c2dd447ac2240b2355
-size 2694
+oid sha256:54131237c8cc248c2e21c8a6db86c31018715e760909f2d222e1e2ebd266fdad
+size 2867
