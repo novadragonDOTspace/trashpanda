d05da3ccfc484180499be3a4f771a75e03254b3a
NovaDragon <me@novadragon.space>
2026-01-31 08:01:20 +0100

    Very basic project structure

---
+++ scripts/GameState.gd
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:5e61bab1cb0810a2a96ec5669b1f922cfb55df17f29a408f92f99b56851291ec
+size 4101
---
+++ scripts/Uility/BigCat.gd
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:bf037a555c51cce204df825e64a5b4706737e4728fabe2e0d435e2ec57a0b5c8
+size 26902
