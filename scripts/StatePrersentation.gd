bc2bad3dba3002cdb16656ca6e0c1fdc273e5012
Richard Wepner <richard.wepner@gmx.net>
2026-01-31 17:10:02 +0100

    minor adjustments

--- scripts/StatePrersentation.gd
+++ scripts/StatePrersentation.gd
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:c147264221dda1d7cbd012b5a95bda8117ae973ad2f89c3712e7ece3e1f5e9df
-size 1512
+oid sha256:0840b727f1f074c0a329583c331cf83fc63bf8aef61f7aab08c9ffe0cbf7d7cc
+size 1540
