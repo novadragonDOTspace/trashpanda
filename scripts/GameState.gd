eaac7c9b8865652ab3071fbc2a32588cd7a1e270
NovaDragon <me@novadragon.space>
2026-01-31 14:52:20 +0100

    UI stuffs

--- scripts/GameState.gd
+++ scripts/GameState.gd
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:5995db6e513166b67d0c56d52894bb3ab57593a1dc8b9fbdecdaa4ca0d3923cb
-size 2168
+oid sha256:9aab55aee79e85baec7b79736a73f67d04c3b93ec806a5b89704a4505db349d2
+size 2205
--- scripts/Racoon.gd
+++ scripts/Racoon.gd
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:bcd54bb192bc92f71d4604a257595215de31dc581a14d17ea24b06f67bd6de1d
-size 534
+oid sha256:a49d48ee521848576ecb7fc203aec6c8fa2c2a4999e91e6db6d90224506123b2
+size 598
