2f6afb1264aed18a4009e11ad6207352fc8496c8
Richard Wepner <richard.wepner@gmx.net>
2026-01-31 17:54:08 +0100

    improved camera scrolling (decaying velocity after release)

--- scripts/RacoonCamera.gd
+++ scripts/RacoonCamera.gd
@@ -1,8 +1,12 @@
 extends Camera2D
 
+@export
+var velocity_decay: float = 10
+
 var was_mouse_down: bool = false
 var mouse_position_start: Vector2
 var previous_mouse_position: Vector2
+var velocity: Vector2
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
@@ -15,8 +19,14 @@ func _process(delta: float) -> void:
 	if is_mouse_down != was_mouse_down:
 		was_mouse_down = is_mouse_down
 		if is_mouse_down:
+			velocity = Vector2(0, 0)
 			mouse_position_start = get_global_mouse_position()
 			previous_mouse_position = get_global_mouse_position()
+		else:
+			var current_mouse_position = get_global_mouse_position()
+			var mouse_delta = current_mouse_position - mouse_position_start
+			velocity = - (mouse_delta)
+			velocity.y = 0
 	elif is_mouse_down:
 		var current_mouse_position = get_global_mouse_position()
 		var mouse_delta = current_mouse_position - mouse_position_start
@@ -24,6 +34,16 @@ func _process(delta: float) -> void:
 		global_position -= mouse_delta
 		_normalize_position()
 		previous_mouse_position = get_global_mouse_position()
+	else:
+		global_position += velocity
+		_normalize_position()
+		var decay_step = velocity_decay * delta
+		var length = velocity.length()
+		var new_length = length - decay_step
+		if new_length <= 0:
+			velocity = Vector2(0, 0)
+		else:
+			velocity = velocity * new_length / length
 func _normalize_position():
 	if limit_enabled:
 		var position = global_position
