640b1e5d4a7f8547f9810fd7ba820ad29027251f
NovaDragon <me@novadragon.space>
2026-01-31 08:47:00 +0100

    export template, readme, basescene, formatting gamestate,gd, also importing bgm

--- scripts/GameState.gd
+++ scripts/GameState.gd
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:5e61bab1cb0810a2a96ec5669b1f922cfb55df17f29a408f92f99b56851291ec
-size 4101
+oid sha256:9f4dac7ed2dffc6e9ba8caee6b0d91a25406eed74562ba8a5ed882e180ef10a1
+size 4145
