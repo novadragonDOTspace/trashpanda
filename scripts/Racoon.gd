8d494bfa8537dedba3e7e3fe342379a212c2277f
Richard Wepner <richard.wepner@gmx.net>
2026-01-31 12:41:59 +0100

    basics for a racoon manager

---
+++ scripts/Racoon.gd
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:691306aa40e8f7c184699303ae4674fd22981b3883d0417dbadad6604b23c920
+size 415
---
+++ scripts/RacoonManager.gd
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:91bf6b3ef46c927698023e40ebe3572486935f97602373ed933652d1d6352c75
+size 1449
