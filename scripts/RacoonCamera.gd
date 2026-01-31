2e59855e9feaf5f6b482577038fe9410e41da55a
Richard Wepner <richard.wepner@gmx.net>
2026-01-31 18:20:43 +0100

    minor fixes for animation and scene setup

--- scripts/RacoonCamera.gd
+++ scripts/RacoonCamera.gd
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:d99a19c27874571beeb07cd50f5cf9087980faccbaa04cddeb6d1dc3b60402eb
-size 1788
+oid sha256:9a77f2d3eaf4b99af88f5c3f440e4223e6f7b33c6bdb47262576fda1d22f0998
+size 1826
