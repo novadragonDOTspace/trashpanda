2305dea5af99de3470b3f1c81d16aac2f03b062c
Christian Heusser <christian.heusser@districtgames.com>
2026-02-01 16:39:13 +0100

    fixed collected trash popping up with wrong timing

--- scripts/RacoonManager.gd
+++ scripts/RacoonManager.gd
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:3ab8415885e7fb48298b9fd6b0030ae2796cb68152bbcc1d99db9f08602dd634
+oid sha256:1aefee3060bf7ea8227fdc01280c86fd97802e618954dcdfcc57919d42f41c08
 size 9260
