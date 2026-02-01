f13cb7db6cc8f19bdfd469d5c604abc3e6f0f775
Christian Heusser <christian.heusser@districtgames.com>
2026-02-01 16:36:47 +0100

    fixed initial unlock

--- scripts/UI/RacoonlLevellableUpgradeButtonCollection.gd
+++ scripts/UI/RacoonlLevellableUpgradeButtonCollection.gd
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:fa7851b494cdcd66f85d9526f8e3b6b421cc7e91eb62ec647ed6dfff16cfa839
-size 3544
+oid sha256:79bbe6c70c77b23f6cb6c843ff73edb54f29a7876641ea9d30dc13fd13980d75
+size 3371
