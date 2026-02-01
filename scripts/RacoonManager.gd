f954f002007de0a310f20ff2b309382db957a656
Richard Wepner <richard.wepner@gmx.net>
2026-02-01 12:32:43 +0100

    added delivery animation, adjusted start scene

--- scripts/RacoonManager.gd
+++ scripts/RacoonManager.gd
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:40cbdc15198b676a3bfd3277f580973492e360099c1180cb92699a4e3ab592f3
-size 9713
+oid sha256:0105c8525d0d3c52bfb1cb8c88ccd489e12cab3a1fcb32406572315b2d37f31f
+size 9774
