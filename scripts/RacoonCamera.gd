d2edf73ae4434b7c8acec2f80b1d614a165f822b
Richard Wepner <richard.wepner@gmx.net>
2026-02-01 15:38:52 +0100

    improved camera scrolling (deal with UI interaction)

--- scripts/RacoonCamera.gd
+++ scripts/RacoonCamera.gd
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:9a77f2d3eaf4b99af88f5c3f440e4223e6f7b33c6bdb47262576fda1d22f0998
-size 1826
+oid sha256:d14ebe6a1571d4704fd6b349b02b8fddd5b270dc692bfdec60a3f3133929a935
+size 1977
