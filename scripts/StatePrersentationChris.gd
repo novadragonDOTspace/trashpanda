4c0e165b11cbb317a8485811b889f30d1fbeceaf
Christian Heusser <christian.heusser@districtgames.com>
2026-02-01 16:49:41 +0100

    added cheat button

--- scripts/StatePrersentationChris.gd
+++ scripts/StatePrersentationChris.gd
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:28ccea219eaee2e4614af3ba9dbacc0bd01498a4df1044485d6f23072bcdbb80
-size 1932
+oid sha256:701ac6e9eb2122f619e7684435fb110d4f85dd9e5713370f956bfb10d42b76f3
+size 2037
